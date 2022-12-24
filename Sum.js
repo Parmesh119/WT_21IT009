031fb12edea8606837b212a821dd31e6d3d91390
Parmesh119 <parmeshb90@gmail..com>
2022-12-24 15:31:15 +0530

    Update Sum.js file

--- Sum.js
+++ Sum.js
@@ -1,6 +1,6 @@
 const prompt = require('prompt-sync')()
-let n1=Number(prompt("Enter the first number"))
-let n2=Number(prompt("Enter the first number"))
+let n1=parseInt(prompt("Enter the first number"))
+let n2=parseInt(prompt("Enter the first number"))
 alert("NUMBER")
 let sum=0
 sum=n1+n2
